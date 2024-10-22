37ee7b0711d9e75007356d8e530d655a5721e81c
Aditya Sharma <124522259+adityasharma149@users.noreply.github.com>
2024-10-23 01:55:06 +0530

    Create Variables.js

---
+++ DAY1/Variables.js
@@ -0,0 +1,32 @@
+//Variables are containers for storing values.//
+
+//Declaring different variables of different data types
+
+let firstName = 'Aditya'
+let lastName = 'Sharma'
+let age = '20'
+let isMarried = false
+
+//Declaring variables with number value
+
+const PI = 3.14
+const gravity = 9.81
+
+//variables can also declare in one line seperated by coma
+
+let name = 'Aditya',city = 'Bhiwadi',age = '20'
+
+//EXAMPLE
+const price1=5;
+const price2=7;
+let total = price1 + price2;
+//Output=> 12
+
+// When to Use var, let, or const?
+// 1. Always declare variables
+
+// 2. Always use const if the value should not be changed
+
+// 3. Always use const if the type should not be changed (Arrays and Objects)
+
+// 4. Only use let if you can't use const
